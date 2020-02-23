e2a00710ef220f671bac7e31cac4502dc1a41c1a
karolis <karolis.stulgys@gmail.com>
2020-02-23 21:41:08 +0200

    add title

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
+      title={"Home"}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
